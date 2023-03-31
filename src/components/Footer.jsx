7c8323fa106a94e24b83e4a310ac9b851d8c9a77
mandil malla <mallamandilcoding@gmail.com>
2023-03-31 12:28:17 +0900

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <div>
+      <h1>TechStar</h1>
+      <p>@all right reserved</p>
+    </div>
+    <div>
+      <h5>Follow Us</h5>
+      <div>
+        <a href="www.youtube.com" target={"_blank"}>Youtube</a>
+        <a href="www.instragram.com" target={"_blank"}>Instragram</a>
+        <a href="www.twitter.com" target={"_blank"}>Twitter</a>
+      </div>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer
