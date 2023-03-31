c64b9d569264e7f5e7f8728803d79c26afbf4799
mandil malla <mallamandilcoding@gmail.com>
2023-03-31 11:46:53 +0900

    home3 updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,6 +18,12 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <div className="home3">
+        <div>
+          <h1>Who we are?</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis optio, aut sapiente doloribus sequi, delectus beatae unde ab provident illo perspiciatis neque omnis voluptates praesentium. Voluptas neque consequatur quae ab. Modi nulla quas deserunt veritatis nihil facere a consequuntur blanditiis inventore, hic quidem autem. Possimus obcaecati cupiditate odit amet quasi, quibusdam porro, inventore dignissimos ut doloremque harum commodi numquam, incidunt natus quo dolor nostrum voluptates ad unde eius consequuntur? Nesciunt reprehenderit quas nisi eos natus! Nihil itaque laborum esse recusandae ab sint? Dolorum veniam dignissimos consectetur ab, distinctio non, fugit sed repellendus explicabo odio cumque quasi tempore omnis dolores voluptate?</p>
+        </div>
+      </div>
     </>
   )
 }
